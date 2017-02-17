94d365d47617e6a48851c38e277b68059aeeaaef
fuming <fuming@pipapai.com>
2017-02-17 16:10:18 +0800

    修改上传的图片文件存放的路径和文件名md5加密

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,15 @@
 # coding: utf-8
 from __future__ import unicode_literals
 from django.utils.encoding import python_2_unicode_compatible
-
 from django.db import models
-
+import hashlib, random, os.path, datetime, time
 # Create your models here.
+def getImageStorePath(self, filename):
+    storepath = self._meta.app_label + "/img/" + datetime.datetime.now().strftime('%Y%m') + "/"
+    self.img = hashlib.md5(str(time.time()) + str(random.randint(0, 999))).hexdigest() + os.path.splitext(filename)[1]
+    print filename
+    return "{storepath}/{filename}".format(storepath=storepath, filename=self.img)
+
 @python_2_unicode_compatible
 class Question(models.Model):
     DISACTIVE = 0
@@ -16,8 +21,10 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200, verbose_name='问题内容')
     desc_text = models.TextField(max_length=500, verbose_name='调查描述', default='', blank=True)
     pub_date = models.DateTimeField('发布时间')
-    img = models.ImageField(upload_to='polls/img/%Y%m', default='', verbose_name='图片地址', blank=True)
+    img = models.ImageField(upload_to=getImageStorePath, default='', verbose_name='图片地址', blank=True)
     status = models.IntegerField(default=1, choices=QUESTION_STATUS_CHOICES, verbose_name='状态')
+
+
 
     def __str__(self):
         return self.question_text
